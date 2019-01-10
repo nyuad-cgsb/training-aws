61e1733c5eed58d00284faee41678fa5757b7f8b
Jillian Rowe <jillian.e.rowe@gmail.com>
2019-01-10 12:17:48 +0300

    noooo more openldap

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker-compose --force-rm build
+docker-compose up -d
---
+++ pre_install_scripts/create_users.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+## This must be run as root, or as a user with elevated permissions
+
+set -x -e
+
+export ANACONDA3_BASE=/home/ec2-user/.local/anaconda3
+export PATH=${ANACONDA3_BASE}/bin:$PATH
+
+python3 ./create_users_file.py
+
+newusers new-users.txt
