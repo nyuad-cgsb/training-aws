32ac5b276d2aa24e4ef725a8948dd69b6b4b68f0
Jillian Rowe <jillian.e.rowe@gmail.com>
2019-01-14 09:27:42 +0300

    updating preinstall

--- pre_install_scripts/install_all_the_things.sh
+++ pre_install_scripts/install_all_the_things.sh
@@ -2,15 +2,14 @@
 
 ## https://raw.githubusercontent.com/nyuad-cgsb/training-aws/feature/user-management/pre_install_scripts/install_all_the_things.sh
 
-echo "HELLO"
-#wget https://raw.githubusercontent.com/nyuad-cgsb/training-aws/feature/user-management/pre_install_scripts/install_lmod.sh
-#chmod 777 install_lmod.sh
-#
-#wget https://raw.githubusercontent.com/nyuad-cgsb/training-aws/feature/user-management/pre_install_scripts/create_users.sh
-#chmod 777 create_users.sh
-#
-#wget https://raw.githubusercontent.com/nyuad-cgsb/training-aws/feature/user-management/pre_install_scripts/create_users_file.py
-#chmod 777 create_users_file.py
+wget https://raw.githubusercontent.com/nyuad-cgsb/training-aws/feature/user-management/pre_install_scripts/install_lmod.sh
+chmod 777 install_lmod.sh
 
-#./install_lmod.sh
-#./create_users.sh
+wget https://raw.githubusercontent.com/nyuad-cgsb/training-aws/feature/user-management/pre_install_scripts/create_users.sh
+chmod 777 create_users.sh
+
+wget https://raw.githubusercontent.com/nyuad-cgsb/training-aws/feature/user-management/pre_install_scripts/create_users_file.py
+chmod 777 create_users_file.py
+
+./install_lmod.sh
+./create_users.sh
