5de8be7513dac6e42b98b2c711cb055a07e874b4
Jillian Rowe <jillian.e.rowe@gmail.com>
2019-01-14 12:42:51 +0300

    changing pre_install to easybuild branch

--- pre_install_scripts/install_all_the_things.sh
+++ pre_install_scripts/install_all_the_things.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
-## https://raw.githubusercontent.com/nyuad-cgsb/training-aws/feature/user-management/pre_install_scripts/install_all_the_things.sh
+## https://raw.githubusercontent.com/nyuad-cgsb/training-aws/feature/easybuild/pre_install_scripts/install_all_the_things.sh
 
-wget https://raw.githubusercontent.com/nyuad-cgsb/training-aws/feature/user-management/pre_install_scripts/install_lmod.sh
+wget https://raw.githubusercontent.com/nyuad-cgsb/training-aws/feature/easybuild/pre_install_scripts/install_lmod.sh
 chmod 777 install_lmod.sh
 
-wget https://raw.githubusercontent.com/nyuad-cgsb/training-aws/feature/user-management/pre_install_scripts/create_users.sh
+wget https://raw.githubusercontent.com/nyuad-cgsb/training-aws/feature/easybuild/pre_install_scripts/create_users.sh
 chmod 777 create_users.sh
 
-wget https://raw.githubusercontent.com/nyuad-cgsb/training-aws/feature/user-management/pre_install_scripts/create_users_file.py
+wget https://raw.githubusercontent.com/nyuad-cgsb/training-aws/feature/easybuild/pre_install_scripts/create_users_file.py
 chmod 777 create_users_file.py
+
+wget https://raw.githubusercontent.com/nyuad-cgsb/training-aws/feature/easybuild/pre_install_scripts/mount_storage.sh
+chmod 777 mount_storage.sh
 
 ./install_lmod.sh
 ./create_users.sh
+./mount_storage.sh
