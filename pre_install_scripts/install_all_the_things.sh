7d4bde110d6cddb24b16f2471bdd1a20bdc36a64
Jillian Rowe <jillian.e.rowe@gmail.com>
2019-01-14 08:27:44 +0300

    trying to get post install to work

--- pre_install_scripts/install_all_the_things.sh
+++ pre_install_scripts/install_all_the_things.sh
@@ -2,14 +2,15 @@
 
 ## https://raw.githubusercontent.com/nyuad-cgsb/training-aws/feature/user-management/pre_install_scripts/install_all_the_things.sh
 
-wget https://raw.githubusercontent.com/nyuad-cgsb/training-aws/feature/user-management/pre_install_scripts/install_lmod.sh
-chmod 777 install_lmod.sh
+echo "HELLO"
+#wget https://raw.githubusercontent.com/nyuad-cgsb/training-aws/feature/user-management/pre_install_scripts/install_lmod.sh
+#chmod 777 install_lmod.sh
+#
+#wget https://raw.githubusercontent.com/nyuad-cgsb/training-aws/feature/user-management/pre_install_scripts/create_users.sh
+#chmod 777 create_users.sh
+#
+#wget https://raw.githubusercontent.com/nyuad-cgsb/training-aws/feature/user-management/pre_install_scripts/create_users_file.py
+#chmod 777 create_users_file.py
 
-wget https://raw.githubusercontent.com/nyuad-cgsb/training-aws/feature/user-management/pre_install_scripts/create_users.sh
-chmod 777 create_users.sh
-
-wget https://raw.githubusercontent.com/nyuad-cgsb/training-aws/feature/user-management/pre_install_scripts/create_users_file.py
-chmod 777 create_users_file.py
-
-./install_lmod.sh
-./create_users.sh
+#./install_lmod.sh
+#./create_users.sh
