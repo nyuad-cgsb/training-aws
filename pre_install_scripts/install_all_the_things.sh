cb6e60578ffcfca9f275bcb38c88569fdfc0b169
Jillian Rowe <jillian.e.rowe@gmail.com>
2019-01-14 07:00:02 +0300

    adding in a wrapper script

---
+++ pre_install_scripts/install_all_the_things.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+wget https://raw.githubusercontent.com/nyuad-cgsb/training-aws/feature/user-management/pre_install_scripts/install_lmod.sh
+chmod 777 install_lmod.sh
+
+wget https://raw.githubusercontent.com/nyuad-cgsb/training-aws/feature/user-management/pre_install_scripts/create_users.sh
+chmod 777 create_users.sh
+
+wget https://raw.githubusercontent.com/nyuad-cgsb/training-aws/feature/user-management/pre_install_scripts/create_users_file.py
+chmod 777 create_users_file.py
+
+./install_lmod.sh
+./create_users.sh
